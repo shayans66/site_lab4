bc29f7bca83a6b02cbd4bf51f4d53ba2e9a7a081
shayans66 <35939705+shayans66@users.noreply.github.com>
2021-06-14 14:38:26 -0700

    endpoints done

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const express = require("express")
 const morgan = require("morgan")
-const votingRouter = require('./routes/voting')
+const giftexchangeRouter = require('./routes/gift-exchange')
 
 const app = express()
 
 app.use(morgan("tiny"))
 app.use(express.json())
 
-app.use('/voting', votingRouter)
+app.use('/gift-exchange', giftexchangeRouter)
 
-// app.use(express.urlencoded());
-
-// const bodyParser  = require('body-parser');
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
 
 app.get("/", async (req, res, next) => {
   res.status(200).json({ping: "pong"})
